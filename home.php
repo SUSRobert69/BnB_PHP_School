b1d47e95b8f2d7bc6373241238c42a1c47c74e16
Roberto Holban <91724440+SUSRobert69@users.noreply.github.com>
2023-03-09 10:46:12 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <title>Benvenuti al Bed & Breakfast</title>
+  <link rel="stylesheet" href="style/home.css">
+</head>
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="prenotazioni.php">Prenotazioni</a></li>
+        <li><a href="amministrazione.php">Amministrazione</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <section id="hero">
+      <h1>Benvenuti al Bed & Breakfast</h1>
+      <p>Godetevi un soggiorno indimenticabile in una delle nostre confortevoli camere. Siamo situati in una posizione privilegiata, a pochi passi dal centro della città e dalle principali attrazioni turistiche.</p>
+    </section>
+    <section id="camere">
+      <h2>Le nostre camere</h2>
+      <p>Scegliete tra le nostre camere arredate con stile e dotate di ogni comfort:</p>
+      <ul>
+        <li>
+          <img src="camera1.jpg" alt="Camera Ciclamini">
+          <h3>Camera Ciclamini</h3>
+          <p>Camera matrimoniale con balcone privato</p>
+        </li>
+        <li>
+          <img src="camera2.jpg" alt="Camera Rose">
+          <h3>Camera Rose</h3>
+          <p>Camera singola con vista sul giardino</p>
+        </li>
+        <li>
+          <img src="camera3.jpg" alt="Camera Girasoli">
+          <h3>Camera Girasoli</h3>
+          <p>Camera familiare con letti singoli e matrimoniale</p>
+        </li>
+        <li>
+          <img src="camera4.jpg" alt="Camera Peonie">
+          <h3>Camera Peonie</h3>
+          <p>Camera doppia con vista sul mare</p>
+        </li>
+      </ul>
+    </section>
+  </main>
+  <footer>
+    <p>&copy; 2023 Bed & Breakfast. Tutti i diritti riservati.</p>
+  </footer>
+</body>
+</html>
