e67744f2209eaad7b2a8ac316550bd2df9bd3ed6
Roberto Holban <91724440+SUSRobert69@users.noreply.github.com>
2023-03-30 09:36:33 +0200

    Add files via upload

--- main.php
+++ main.php
@@ -1,15 +1,37 @@
 <!DOCTYPE html>
 <html>
   <head>
+  <link rel="stylesheet" href="style.css">
     <title>Benvenuti al Bed & Breakfast</title>
   </head>
   <body>
     <h1>Benvenuti al nostro Bed & Breakfast!</h1>
-    <p>Siamo felici di accogliervi nel nostro B&B, situato in una posizione privilegiata nel cuore della città.</p>
-    <p>Il nostro Bed & Breakfast dispone di camere accoglienti e confortevoli, arredate con cura e dotate di tutti i comfort necessari per rendere il vostro soggiorno indimenticabile.</p>
+    <p>Siamo felici di accogliervi nel nostro B&B, nel cuore della bellissima città di Brescia.</p>
+    <p>Il nostro B&B dispone di camere accoglienti e confortevoli, arredate con cura e dotate di tutti i comfort necessari per rendere il vostro soggiorno indimenticabile.</p>
+    
+
+    <h1>Camera 1: Ciclamini</h1>
+    <div style="text-align: center;">
+  <img src="https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Descrizione dell'immagine" style="width: 50%; max-width: 400px;">
+</div>
+    <h1>Camera 2: Rose</h1>
+    <div style="text-align: center;">
+  <img src="https://images.unsplash.com/photo-1594560913095-8cf34bab82ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" alt="Descrizione dell'immagine" style="width: 50%; max-width: 400px;">
+</div>
+    <h1>Camera 3: Girasoli</h1>
+    <div style="text-align: center;">
+  <img src="https://images.unsplash.com/photo-1566665797739-1674de7a421a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="Descrizione dell'immagine" style="width: 50%; max-width: 400px;">
+</div>
+    <h1>Camera 4: Peonie</h1>
+    <div style="text-align: center;">
+  <img src="https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Descrizione dell'immagine" style="width: 50%; max-width: 400px;">
+</div>
+    
+    
+    
     <p>Potete prenotare la vostra camera utilizzando il link qui sotto:</p>
     <a href="http://localhost:3000/prenotazioni.php">Prenota la tua camera</a>
-    <p>Se siete amministratori del nostro Bed & Breakfast, potete accedere all'area amministrativa tramite il link qui sotto:</p>
+    <p>amministratori:</p>
     <a href="http://localhost:3000/admin.php">Area amministrativa</a>
   </body>
 </html>
